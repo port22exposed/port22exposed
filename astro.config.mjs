a08b06a63dfcffa60e4606ce58a44b86676c49cf
port22exposed <scm@port22.exposed>
2026-04-04 03:02:27 +0100

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap"
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://port22.exposed",
-    output: "static",
-    devToolbar: {
-        enabled: false
-    },
-    integrations: [sitemap()]
+	output: "static",
+	devToolbar: {
+		enabled: false,
+	},
+	integrations: [sitemap()],
 })
